0dd63303c0e0a6c47edfcda9ce3dd4a6674dfd75
pzyweb <782046126@qq.com>
2022-02-19 01:05:57 +0800

    create app

---
+++ src/components/Hello.vue
@@ -0,0 +1,38 @@
+<script lang="tsx">
+import { ref,defineComponent } from 'vue'
+
+
+const count = ref(0)
+export default defineComponent({
+    props: {
+      msg: String
+    },
+    setup(props){
+        return ()=>(
+        <div>
+        Hello,World {count.value}
+        <h1>{props.msg}</h1>
+        </div>
+        
+        )
+    }
+})
+</script>
+
+<style scoped>
+a {
+  color: #42b983;
+}
+
+label {
+  margin: 0 0.5em;
+  font-weight: bold;
+}
+
+code {
+  background-color: #eee;
+  padding: 2px 4px;
+  border-radius: 4px;
+  color: #304455;
+}
+</style>
